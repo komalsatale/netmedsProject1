d5a4b279b519292d059ef09d14bd1a4a3a4b1af4
komalsatale <123380647+komalsatale@users.noreply.github.com>
2024-08-08 22:42:51 +0530

    useMemo component is added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Home from './components/Home';
 import PropsDrilling from './components/props_drilling/PropsDrilling';
+import UseMemo from './components/hooks/UseMemo';
 
 function App(props) {
   return (
     <div>
       <Home/>
       <PropsDrilling/>
+      <UseMemo/>
     </div>
   );
 }
