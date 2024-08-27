aac37bd943579a82337fbf4351bdf0948cf99b5a
tabrejkhanpathan <tabrejpathan308@gmail.com>
2024-08-27 10:53:40 +0530

    code written by class component life cycle method

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
-import Home from './components/Home';
-import PropsDrilling from './components/props_drilling/PropsDrilling';
-import UseMemo from './components/hooks/UseMemo';
+import LifeCycle from './components/classcomponent/LifeCycle';
+// import Home from './components/Home';
+// import PropsDrilling from './components/props_drilling/PropsDrilling';
+// import UseMemo from './components/hooks/UseMemo';
 
 function App(props) {
   return (
     <div>
-      <Home/>
+      {/* <Home/>
       <PropsDrilling/>
-      <UseMemo/>
+      <UseMemo/> */}
+      <LifeCycle/>
+
     </div>
   );
 }
---
+++ src/components/classcomponent/LifeCycle.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class LifeCycle extends Component {
+  render() {
+    return (
+      <div>
+        <h1>learn about class Component Life Cycle method</h1>
+      </div>
+    )
+  }
+}
